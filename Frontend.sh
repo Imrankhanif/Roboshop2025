f0961272124195a922540f8148b5fe678247da04
imran <imrankhanmohammed130@gmail.com>
2025-04-11 20:38:29 -0500

    start fronet end

--- Frontend.sh
+++ Frontend.sh
@@ -1,16 +1,22 @@
+echo -e "\e[33m Installing Nginx server\e[0m"
 yum install nginx -y 
 
 
 
-
+echo -e "\e[33m Removing old app content\e[0m"
 rm -rf /usr/share/nginx/html/* 
 
+echo -e "\e[33m Downloadig frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
+
+echo -e "\e[33m Extract frontend content\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
 
 #we need to copy config file
+
+echo -e "\e[33m Starting nginx server\e[0m"
 systemctl enable nginx 
 systemctl restart nginx 
